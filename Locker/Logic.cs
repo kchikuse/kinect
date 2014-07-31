c5324a22e619a4fdcb1ffaa4cc0f6a40bf9f7e9f
K Chikuse <kchikuse@gmail.com>
2014-07-31 14:07:26 +0200

    Simple house bot
    Uses a kinect to sense presense and do tasks

---
+++ Locker/Logic.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Linq;
+using System.Speech.Synthesis;
+using System.Threading;
+using iTunesLib;
+using Timer = System.Timers.Timer;
+
+namespace Locker
+{
+    public class Logic
+    {
+        static bool armed;
+        static Timer resetTimer;
+        static iTunesApp itunes;
+        static SpeechSynthesizer speaker;
+
+        public Logic()
+        {
+            armed = true;
+
+            resetTimer = new Timer();
+            itunes = new iTunesApp();
+            speaker = new SpeechSynthesizer();
+
+            resetTimer.Interval = TimeSpan.FromMinutes(30).TotalMilliseconds;
+            resetTimer.Elapsed += (o, e) =>
+                                      {
+                                          armed = true;
+                                          resetTimer.Stop();
+                                      };
+        }
+
+        public string Process(int active, int passive)
+        {
+            if (armed)
+            {
+                if(active > 0)
+                {
+                    armed = false;
+                    resetTimer.Start();
+                    Speak("Welcome back Knowledge.");
+
+                    if(active == 2)
+                    {
+                        Stop(4);
+                        Speak("{0} visitor. My name is Bob. Welcome to our house", Greet());
+                    }
+
+                    Play();
+                }
+            }
+
+            Console.ForegroundColor = Color(active);
+
+            return string.Format("{0:h:mm:ss} (ACTIVE) = {1}, (PASSIVE) = {2}", DateTime.Now, active, passive);
+        }
+        
+        public ConsoleColor Color(int active)
+        {
+            return active > 0 ? ConsoleColor.Red : ConsoleColor.White;
+        }
+
+        static string Greet()
+        {
+            var greeting = "";
+            var hour = DateTime.Now.Hour;
+
+            if (hour >= 5 && hour < 12)
+            {
+                greeting = "Good morning";
+            }
+
+            if (hour >= 12 && hour < 18)
+            {
+                greeting = "Good afternoon";
+            }
+
+            if (hour >= 18 && hour < 24)
+            {
+                greeting = "Good evening";
+            }
+
+            return greeting;
+        }
+
+        static void Play()
+        {
+            var relax = FindPlayList("Surf Roots");
+            if (relax != null)
+            {
+                Stop(2);
+                relax.PlayFirstTrack();
+            }
+        }
+        
+        static void Stop(int seconds)
+        {
+            Thread.Sleep(seconds * 1000);
+        }
+
+        static IITPlaylist FindPlayList(string name)
+        {
+            return itunes.LibrarySource.Playlists.Cast<IITPlaylist>().FirstOrDefault(i => i.Name == name);
+        }
+
+        static void Speak(string text, params object[] args)
+        {
+            speaker.Speak(string.Format(text, args));
+        }
+    }
+}
---
+++ Locker/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Timers;
+using Microsoft.Kinect;
+
+namespace Locker
+{
+    class Program
+    {
+        static Timer timer;
+        static Logic logic;
+        static KinectSensor sensor;
+
+        static void Main()
+        {
+            Init();
+
+            try
+            {
+                logic = new Logic();
+                sensor = KinectSensor.KinectSensors[0];
+
+                sensor.Start();
+                sensor.SkeletonStream.Enable();
+                sensor.SkeletonStream.EnableTrackingInNearRange = true;
+
+                timer = new Timer();
+                timer.Interval = 500;
+                timer.Elapsed += Timer_Elapsed;
+                timer.Enabled = true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            Console.ReadLine();
+        }
+
+        static void Init()
+        {
+            Console.Title = "BOB v3";
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.Clear();
+        }
+        
+        static void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            var passive = 0;
+            var active = 0;
+
+            foreach(var k in KinectSensor.KinectSensors)
+            {
+                using(var frame = k.SkeletonStream.OpenNextFrame(50))
+                {
+                    if(frame != null)
+                    {
+                        var s = new Skeleton[frame.SkeletonArrayLength];
+                        frame.CopySkeletonDataTo(s);
+                        foreach (var skeleton in s)
+                        {
+                            if (skeleton.TrackingState == SkeletonTrackingState.Tracked)
+                                active++;
+
+                            if (skeleton.TrackingState == SkeletonTrackingState.PositionOnly)
+                                passive++;
+                        }
+                    }
+                }
+
+                Console.WriteLine(logic.Process(active, passive));
+            }
+        }
+    }
+}
